472841a2ec9e32fc6d6ac76f3ac56be5bf2e1bd0
maharaja10 <153357648+maharaja10@users.noreply.github.com>
2025-01-31 09:31:39 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { ChakraProvider, Box } from '@chakra-ui/react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Colleges from './pages/Colleges';
+import Tutors from './pages/Tutors';
+import Feedback from './pages/Feedback';
+import FeedbackDetails from './pages/FeedbackDetails';
+import FeedbackForm from './pages/feedbackform';
+import Login from './pages/Login';
+function App() {
+  return (
+    <ChakraProvider>
+      <Router>
+        <MainLayout />
+      </Router>
+    </ChakraProvider>
+  );
+}
+
+function MainLayout() {
+  const location = useLocation(); 
+  const isFeedbackFormPage = location.pathname.startsWith('/feedbackform/'); 
+  const isHomePage = location.pathname === '/';
+  return (
+    <Box display="flex">
+      {!isFeedbackFormPage && !isHomePage && <Sidebar />}
+      <Box flex="1" p={5}>
+        <Routes>
+          <Route path="/college" element={<Colleges />} />
+          <Route path="/tutors" element={<Tutors />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/feedback/:id" element={<FeedbackDetails />} />
+          <Route path="/feedbackform/:id" element={<FeedbackForm />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Box>
+    </Box>
+  );
+}
+
+
+export default App;
